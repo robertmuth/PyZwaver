af54f2a78232799cdfa7493f45785cf0685bdc7b
Robert Muth <robert@muth.org>
2018-08-25 12:33:42 -0400

    fixup setup.py to reflect new file

--- pyzwaver/__init__.py
+++ pyzwaver/__init__.py
@@ -1,12 +1,20 @@
 
 from . import command
+from . import command_helper
 from . import command_translator
 from . import controller
 from . import driver
 from . import node
 from . import value
 from . import zmessage
-from . import zsecurity
 from . import zwave
 
-__all__ = ['command', 'command_translator', 'controller', 'driver', 'node', 'value', 'zmessage', 'zsecurity', 'zwave']
+__all__ = ['command',
+           'command_helper',
+           'command_translator',
+           'controller',
+           'driver',
+           'node',
+           'value',
+           'zmessage',
+           'zwave']
